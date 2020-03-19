a414c4c78a14e01e492f5d49ec1fe6f0080a411b
MydroX <maxime@zinutti.fr>
2020-03-20 00:46:56 +0100

    Get party and display data of it

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res) {
+
+app.get('/', function (req, res) {
     res.render("index", {title: "Qui prend quoi ?"});
+});
+app.post('/party', function (req, res) {
+    axios
+        .post(`${process.env.API_URL}/party`, req.body)
+        .then(({ data }) => res.redirect(`/party/${data._id}`))
+        .catch((err) => res.send(err));
+});
+app.get('/party/:id', function (req, res) {
+    axios
+        .get(`${process.env.API_URL}/party/${req.params.id}`)
+        .then(({data}) =>
+            res.render('party', {
+                party: data,
+                title: data.name,
+                url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`
+            }),
+        )
+        .catch((err) => console.log(err));
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
